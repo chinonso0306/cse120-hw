25c64309ba7e854cc947e0fe1f5f25891f1e3eeb
chinonso daniel <chinonsodanielechefu@gmail.com>
2026-03-07 12:47:33 +0100

    Added week 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Ask the user for their grade percentage
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+
+        int grade = int.Parse(input);
+        string letter = "";
+
+        // Determine the letter grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Print the letter grade
+        Console.WriteLine($"Your grade is: {letter}");
+
+        // Determine pass or fail
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You will do better next time.");
+        }
     }
 }
